d24d5e2d93c733d6d17f531d5bfdd0e5a93d4109
Denim Datta 👻 <d.dat08@gmail.com>
2025-06-22 12:26:00 -0700

    Move Dummy Component

--- src/HelloWorld.tsx
+++
@@ -1,7 +0,0 @@
-function HelloWorld() {
-    return (
-        <h1 style={{textAlign: 'center'}}>Denim Datta</h1>
-    );
-}
-
-export default HelloWorld;
---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,9 @@
+function HelloWorld() {
+    return (
+        <div className='row'>
+            <h1 style={{textAlign: 'center'}}>Denim Datta</h1>
+        </div>
+    );
+}
+
+export default HelloWorld;
