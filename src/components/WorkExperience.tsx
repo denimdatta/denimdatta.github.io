2a55ecdc05df8c239e6e02f028af25b77c9befcb
Denim Datta 👻 <d.dat08@gmail.com>
2025-06-26 07:34:05 -0700

    Add Experience Section | PlaceHolder

---
+++ src/components/WorkExperience.tsx
@@ -0,0 +1,13 @@
+function WorkExperience() {
+    return (
+        <section id ="work"
+            className="relative px-4 py-24">
+            <div className="row text-center">
+                <h1>Professional Experience</h1>
+                <h3>Placeholder</h3>
+            </div>
+        </section>
+    );
+}
+
+export default WorkExperience;
