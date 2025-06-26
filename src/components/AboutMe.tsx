1b2406f4f97e6bd158a72c2c87ff647761683e52
Denim Datta 👻 <d.dat08@gmail.com>
2025-06-26 07:24:48 -0700

    Add AboutMe Section | PlaceHolder

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,13 @@
+function AboutMe() {
+    return (
+        <section id ="about"
+            className="relative px-4 py-24">
+            <div className="row text-center">
+                <h1>About Me</h1>
+                <h3>Placeholder</h3>
+            </div>
+        </section>
+    );
+}
+
+export default AboutMe;
