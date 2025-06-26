1b76e1b44f85e56116b9dd8212261a8d1dbe33f5
Denim Datta 👻 <d.dat08@gmail.com>
2025-06-26 07:25:05 -0700

    Add Skills Section | PlaceHolder

---
+++ src/components/Skills.tsx
@@ -0,0 +1,13 @@
+function Skills() {
+    return (
+        <section id ="skills"
+            className="relative px-4 py-24">
+            <div className="row text-center">
+                <h1>Skills</h1>
+                <h3>Placeholder</h3>
+            </div>
+        </section>
+    );
+}
+
+export default Skills;
