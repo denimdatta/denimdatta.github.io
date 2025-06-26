6a251951b9c55753fb8188afa6e653d0a225d287
Denim Datta 👻 <d.dat08@gmail.com>
2025-06-26 07:34:19 -0700

    Add Projects Section | PlaceHolder

---
+++ src/components/Projects.tsx
@@ -0,0 +1,13 @@
+function Projects() {
+    return (
+        <section id ="projects"
+            className="relative px-4 py-24">
+            <div className="row text-center">
+                <h1>Projects</h1>
+                <h3>Placeholder</h3>
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
