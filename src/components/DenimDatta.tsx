6f8ec488dc4923291eee34dd6ac4795c8232e037
Denim Datta 👻 <d.dat08@gmail.com>
2025-06-29 23:12:33 -0700

    Add Scroll Arrow for Home Page

--- src/components/DenimDatta.tsx
+++ src/components/DenimDatta.tsx
@@ -1,9 +1,17 @@
+import {SlArrowDown} from "react-icons/sl";
+import {CN} from "../lib/Utils";
 function DenimDatta() {
     return (
         <section id ="denimdatta"
             className="relative min-h-screen flex flex-col items-center justify-center px-4">
             <div className='row'>
                 <h1 style={{textAlign: 'center'}}>Denim Datta</h1>
+            </div>
+            <div className={CN("absolute bottom-8 left-1/2 transform -translate-x-1/2",
+                "flex flex-col items-center animate-bounce",
+            )}>
+                <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
+                <SlArrowDown className="h-8 w-8 text-primary" />
             </div>
         </section>
     );
