d78d9d1524b2eae459f85c3a7ed4a070e13c8c98
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-01 16:49:25 -0700

    Add Test for App Component

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      tailwindcss(),
-      react()
-  ],
+    plugins: [
+        tailwindcss(),
+        react()
+    ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: "./tst/setup.ts",
+    }
 })
