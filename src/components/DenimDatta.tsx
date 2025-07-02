02db6e6f88403583de69799c4e7272e961d43e08
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-01 19:56:02 -0700

    Update Home Section

--- src/components/DenimDatta.tsx
+++ src/components/DenimDatta.tsx
@@ -2,9 +2,25 @@ import {useEffect, useState} from "react";
 import {SlArrowDown} from "react-icons/sl";
 import {CN} from "../lib/Utils";
 import Typewriter from "typewriter-effect";
+import {FaAmazon, FaGitAlt, FaGitkraken, FaJava} from "react-icons/fa";
 
 function DenimDatta() {
     const [isScrolled, setIsScrolled] = useState(false);
+    const wavyName = (
+        <>
+            <span className={CN("animate-wave-1 inline-block w-7 text-primary relative")}>D</span>
+            <span className={CN("animate-wave-2 inline-block w-7 text-primary relative")}>E</span>
+            <span className={CN("animate-wave-3 inline-block w-7 text-primary relative")}>N</span>
+            <span className={CN("animate-wave-4 inline-block w-7 text-primary relative")}>I</span>
+            <span className={CN("animate-wave-5 inline-block w-7 text-primary relative")}>M</span>
+            <span className={CN("inline-block w-10 relative")}/>
+            <span className={CN("animate-wave-1 inline-block w-7 text-foreground relative")}>D</span>
+            <span className={CN("animate-wave-2 inline-block w-7 text-foreground relative")}>A</span>
+            <span className={CN("animate-wave-3 inline-block w-7 text-foreground relative")}>T</span>
+            <span className={CN("animate-wave-4 inline-block w-7 text-foreground relative")}>T</span>
+            <span className={CN("animate-wave-5 inline-block w-7 text-foreground relative")}>A</span>
+        </>
+    );
 
     useEffect(() => {
         console.log(`DENIM:: ${isScrolled} :: ${window.scrollY}`)
@@ -19,19 +35,22 @@ function DenimDatta() {
     return (
         <section id ="denimdatta"
             className="relative min-h-screen flex flex-col items-center justify-center px-4">
-            <div className='row pb-10'>
-                <h1 className="text-center">
-                    <span className={CN("animate-wave-1 inline-block w-7 text-primary")}>D</span>
-                    <span className={CN("animate-wave-2 inline-block w-7 text-primary")}>E</span>
-                    <span className={CN("animate-wave-3 inline-block w-7 text-primary")}>N</span>
-                    <span className={CN("animate-wave-4 inline-block w-7 text-primary")}>I</span>
-                    <span className={CN("animate-wave-5 inline-block w-7 text-primary")}>M</span>
-                    <span className={CN("inline-block w-10")}/>
-                    <span className={CN("animate-wave-1 inline-block w-7 text-foreground")}>D</span>
-                    <span className={CN("animate-wave-2 inline-block w-7 text-foreground")}>A</span>
-                    <span className={CN("animate-wave-3 inline-block w-7 text-foreground")}>T</span>
-                    <span className={CN("animate-wave-4 inline-block w-7 text-foreground")}>T</span>
-                    <span className={CN("animate-wave-5 inline-block w-7 text-foreground")}>A</span>
+
+            {/*Desktop Screen*/}
+            <div className='row pb-10 gap-4 w-[75%] grid-cols-12 hidden md:flex md:visible'>
+                <FaGitAlt className="col-1 h-10 animate-appear hidden md:flex md:visible"/>
+                <FaJava className="col-1 h-10 animate-appear hidden md:flex md:visible"/>
+                <h1 className="justify-center col-6 gap-4 relative hidden md:flex md:visible">
+                    {wavyName}
+                </h1>
+                <FaAmazon className="col-1 h-10 animate-appear hidden md:flex md:visible"/>
+                <FaGitkraken className="col-1 h-10 animate-appear hidden md:flex md:visible"/>
+            </div>
+
+            {/*Mobile Screen*/}
+            <div className='md:hidden row pb-10 gap-4'>
+                <h1 className="md:hidden justify-center inline-block gap-4 relative">
+                    {wavyName}
                 </h1>
             </div>
 
