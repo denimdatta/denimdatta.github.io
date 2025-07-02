45dace0114c454df3f5e1d6cf2e72e5b1cbd1b47
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-01 17:45:56 -0700

    Add Code Coverage

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,12 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         setupFiles: "./tst/setup.ts",
+        coverage: {
+            provider: 'v8', // or 'istanbul'
+            reporter: ['text', 'json', 'html'], // Reporters for output formats
+            // Optional: Specify files to include/exclude from coverage report
+            include: ['src/**/*.{ts,tsx}'],
+            exclude: ['src/main.tsx'],
+        },
     }
 })
