aeb7fc6c91f48d4b11205625152d6c8dac3f05c4
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-04 05:14:57 -0700

    Add Placeholder Structure for Career

--- src/components/FlipCard.tsx
+++ src/components/FlipCard.tsx
@@ -1,9 +1,28 @@
-
-function FlipCard() {
-	return (
-		<>
-		</>
-	)
+interface FlipCardProps {
+	id: string;
+	frontContent: React.ReactNode;
+	backContent: React.ReactNode;
 }
+
+const FlipCard: React.FC<FlipCardProps> = ({id, frontContent, backContent}) => {
+	return (
+		<div key={id} className="group h-96 [perspective:1000px]">
+			<div className="flip-card-wrapper">
+				{/* Front Face */}
+				<div className="flip-card-front">
+					<div className="flex min-h-full flex-col items-center justify-center">
+						{frontContent}
+					</div>
+				</div>
+				{/* Back Face */}
+				<div className="flip-card-back">
+					<div className="flex min-h-full flex-col items-start justify-center">
+						{backContent}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+};
 
 export default FlipCard;
--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,4 +1,47 @@
 import {CN} from "../lib/Utils";
+import FlipCard from "./FlipCard";
+
+const experienceElements = [
+	{
+		"name": "PH1",
+		"frontContent": (
+			<>
+				<h4>Placeholder 1</h4>
+			</>
+		),
+		"backContent": (
+			<>
+				<p>Placeholder 1 Content</p>
+			</>
+		),
+	},
+	{
+		"name": "PH2",
+		"frontContent": (
+			<>
+				<h4>Placeholder 2</h4>
+			</>
+		),
+		"backContent": (
+			<>
+				<p>Placeholder 2 Content</p>
+			</>
+		),
+	},
+	{
+		"name": "PH3",
+		"frontContent": (
+			<>
+				<h4>Placeholder 3</h4>
+			</>
+		),
+		"backContent": (
+			<>
+				<p>Placeholder 3 Content</p>
+			</>
+		),
+	},
+];
 
 function WorkExperience() {
 	return (
@@ -7,8 +50,10 @@ function WorkExperience() {
 				<h2 className={CN("text-3xl md:text-4xl font-bold mb-12 text-center")}>
 					Work <span className="text-primary">Experience</span>
 				</h2>
-				<div className="py-8">
-					<h3>Placeholder</h3>
+				<div className={CN("grid grid-cols-1 md:grid-cols-3 gap-12 items-center py-8")}>
+					{experienceElements.map((exp, index) => (
+						<FlipCard key={index} frontContent={exp.frontContent} backContent={exp.backContent} id={exp.name}/>
+					))}
 				</div>
 			</div>
 		</section>
