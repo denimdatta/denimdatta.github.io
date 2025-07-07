0a63d045158b87e38e56fcadf359016593d93965
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-07 12:56:32 -0700

    Test | Footer Component

--- tst/components/Footer.test.tsx
+++ tst/components/Footer.test.tsx
@@ -1,11 +1,18 @@
 import {render} from '@testing-library/react'
-import {describe, it} from "vitest";
+import {describe, expect, it} from "vitest";
 import Footer from "../../src/components/Footer";
 
 describe('Footer', () => {
-    it('renders the Footer component', () => {
-        render(<Footer/>)
+	it('renders the Footer component (Default)', () => {
+		const renderResult = render(<Footer/>)
 
-        screen.debug(); // prints out the jsx in the App component unto the command line
-    });
-})
+		const date = new Date();
+		const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+		const dateStr = date.toDateString().substring(4);
+		const day = weekday[date.getDay()];
+
+		expect(renderResult.baseElement.innerHTML).toContain(dateStr);
+		expect(renderResult.baseElement.innerHTML).toContain("Denim Datta");
+		expect(renderResult.baseElement.innerHTML).toContain(day);
+	});
+});
