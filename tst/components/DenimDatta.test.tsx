c968f637828277c4269db6c611f2f2cca481a4c7
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-07 14:58:26 -0700

    Test | Home Component

--- tst/components/DenimDatta.test.tsx
+++ tst/components/DenimDatta.test.tsx
@@ -1,11 +1,67 @@
-import {render, screen} from '@testing-library/react'
-import {describe, it} from "vitest";
+import {act, render, RenderResult} from '@testing-library/react'
+import {describe, expect, it} from "vitest";
 import DenimDatta from "../../src/components/DenimDatta";
 
 describe('DenimDatta', () => {
-	it('renders the DenimDatta component', () => {
-		render(<DenimDatta/>)
+	it('renders the DenimDatta component (Default)', () => {
+		const renderResult = render(<DenimDatta/>);
 
-		screen.debug(); // prints out the jsx in the App component unto the command line
+		const scroll = renderResult.getByText("Scroll").parentElement;
+		expect(scroll.outerHTML).toContain("animate-bounce");
+		expect(scroll.outerHTML).not.toContain("animate-bounce hidden");
 	});
-})
+
+	it('renders the DenimDatta component (Scroll)', () => {
+		let renderResult: RenderResult;
+
+		act(() => {
+			renderResult = render(<DenimDatta/>);
+		});
+
+		// Initially the Scroll should be present
+		const scroll = renderResult.getByText("Scroll").parentElement;
+		expect(scroll.outerHTML).toContain("animate-bounce");
+		expect(scroll.outerHTML).not.toContain("animate-bounce hidden");
+
+		Object.defineProperty(window, 'scrollY', {value: 200});
+		act(() => window.dispatchEvent(new Event('scroll')));
+
+		// After scrolling beyond a threshold, Scroll should be hidden
+		expect(scroll.outerHTML).toContain("animate-bounce hidden");
+	});
+
+	it('renders the DenimDatta component (Scroll Up/Down)', () => {
+		let renderResult: RenderResult;
+
+		act(() => {
+			renderResult = render(<DenimDatta/>);
+		});
+
+		// Initially the Scroll should be present
+		const scroll = renderResult.getByText("Scroll").parentElement;
+		expect(scroll.outerHTML).toContain("animate-bounce");
+		expect(scroll.outerHTML).not.toContain("animate-bounce hidden");
+
+		// Scroll Down
+		Object.defineProperty(window, 'scrollY', {value: 200});
+		act(() => window.dispatchEvent(new Event('scroll')));
+
+		// After scrolling down, Scroll should be hidden
+		expect(scroll.outerHTML).toContain("animate-bounce hidden");
+
+		// Scroll Up
+		Object.defineProperty(window, 'scrollY', {value: 150});
+		act(() => window.dispatchEvent(new Event('scroll')));
+
+		// After scrolling up, Scroll should be visible
+		expect(scroll.outerHTML).toContain("animate-bounce");
+		expect(scroll.outerHTML).not.toContain("animate-bounce hidden");
+
+		// Scroll Down
+		Object.defineProperty(window, 'scrollY', {value: 151});
+		act(() => window.dispatchEvent(new Event('scroll')));
+
+		// After scrolling down, Scroll should be hidden
+		expect(scroll.outerHTML).toContain("animate-bounce hidden");
+	});
+});
