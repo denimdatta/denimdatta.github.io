10b33910b6a9c05f3b0f2645a7cb9e0f485ab17d
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-07 17:22:19 -0700

    Test | AboutMe Component

--- tst/components/AboutMe.test.tsx
+++ tst/components/AboutMe.test.tsx
@@ -1,11 +1,20 @@
-import {render, screen} from '@testing-library/react'
-import {describe, it} from "vitest";
+import {render} from '@testing-library/react'
+import {describe, expect, it} from "vitest";
 import AboutMe from "../../src/components/AboutMe";
 
 describe('AboutMe', () => {
 	it('renders the AboutMe component', () => {
-		render(<AboutMe/>)
+		render(<AboutMe/>);
 
-		screen.debug(); // prints out the jsx in the App component unto the command line
+		const renderBody = document.querySelector("body");
+
+		expect(renderBody.innerHTML).toContain("Software Development Engineer");
+		expect(renderBody.innerHTML).toContain("Download Resume");
+		expect(renderBody.innerHTML).toContain("Resume.pdf");
+		expect(renderBody.innerHTML).toContain("DenimDatta_Resume");
+
+		expect(renderBody.innerHTML).toContain("Programmer");
+		expect(renderBody.innerHTML).toContain("Software Developer");
+		expect(renderBody.innerHTML).toContain("Computer Science");
 	});
-})
+});
