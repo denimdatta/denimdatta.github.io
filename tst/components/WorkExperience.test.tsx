9eb3e3884fa35cd0b388063da411d33ba09211d6
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-09 12:00:49 -0700

    Test | WorkExperience Component

--- tst/components/WorkExperience.test.tsx
+++ tst/components/WorkExperience.test.tsx
@@ -1,11 +1,25 @@
-import {render, screen} from '@testing-library/react'
-import {describe, it} from "vitest";
+import {render} from '@testing-library/react'
+import {describe, expect, it} from "vitest";
 import WorkExperience from "../../src/components/WorkExperience";
 
 describe('WorkExperience', () => {
-	it('renders the WorkExperience component', () => {
-		render(<WorkExperience/>)
+	it('displays the work experience section title', () => {
+		const renderResult = render(<WorkExperience/>);
 
-		screen.debug(); // prints out the jsx in the App component unto the command line
+		expect(renderResult.getByText((_, element) => element.textContent === 'Work Experience')).toBeInTheDocument();
 	});
-})
+
+	it('displays company name and position', () => {
+		const renderResult = render(<WorkExperience/>);
+
+		expect(renderResult.getByText("Amazon")).toBeInTheDocument();
+		expect(renderResult.getByText("Software Development Engineer")).toBeInTheDocument();
+
+		expect(renderResult.getByText("Aricent")).toBeInTheDocument();
+		expect(renderResult.getByText("Senior Software Engineer")).toBeInTheDocument();
+
+		expect(renderResult.getByText("Microsoft (R&D)")).toBeInTheDocument();
+		expect(renderResult.getByText("Intern - SDET")).toBeInTheDocument();
+	});
+
+});
