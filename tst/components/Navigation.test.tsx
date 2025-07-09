7d9bd47a9fc5dd60244374d52b85a5d50f2880e9
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-09 00:11:48 -0700

    Test | Navigation Component

--- tst/components/Navigation.test.tsx
+++ tst/components/Navigation.test.tsx
@@ -1,11 +1,35 @@
-import {render, screen} from '@testing-library/react'
-import {describe, it} from "vitest";
+import {act, render, RenderResult} from '@testing-library/react'
+import {describe, expect, it} from "vitest";
+import {userEvent} from "@testing-library/user-event";
 import Navigation from "../../src/components/Navigation";
 
 describe('Navigation', () => {
-	it('renders the Navigation component', () => {
-		render(<Navigation/>)
 
-		screen.debug(); // prints out the jsx in the App component unto the command line
+	it('renders navigation links correctly', () => {
+		const renderResult = render(<Navigation/>);
+
+		expect(renderResult.getAllByText("Home").length).toEqual(2);
+		expect(renderResult.getAllByText("About Me").length).toEqual(2);
+		expect(renderResult.getAllByText("Timeline").length).toEqual(2);
+		expect(renderResult.getAllByText("Career").length).toEqual(2);
+		expect(renderResult.getAllByText("Skills").length).toEqual(2);
+		expect(renderResult.getAllByText("Projects").length).toEqual(2);
 	});
-})
+
+	it('toggles mobile menu on button click', async () => {
+		const ue = userEvent.setup();
+		const renderResult = render(<Navigation/>);
+
+		const mobileMenu = renderResult.getByLabelText("Mobile Menu");
+		expect(mobileMenu.outerHTML).toContain("opacity-0");
+
+		const btnMenuOpen = renderResult.getByRole('button', {name: 'Open Menu'});
+		await ue.click(btnMenuOpen);
+
+		expect(mobileMenu.outerHTML).toContain("opacity-100");
+
+		const btnMenuClose = renderResult.getByRole('button', {name: 'Close Menu'});
+		await ue.click(btnMenuClose);
+		expect(mobileMenu.outerHTML).toContain("opacity-0");
+	});
+});
