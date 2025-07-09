3d4118282693e825456313b46d0bf6c425a59fc1
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-09 00:44:41 -0700

    Test | 404 Page

--- tst/pages/Page404.test.tsx
+++ tst/pages/Page404.test.tsx
@@ -1,11 +1,10 @@
-import {render, screen} from '@testing-library/react'
-import {describe, it} from "vitest";
+import {render} from '@testing-library/react'
+import {describe, expect, it} from "vitest";
 import Page404 from "../../src/pages/Page404";
 
 describe('Page404', () => {
 	it('renders the Page404 component', () => {
-		render(<Page404/>)
-
-		screen.debug(); // prints out the jsx in the App component unto the command line
+		const renderResult = render(<Page404/>);
+		expect(renderResult.getAllByText("Page Not Found").length).toEqual(1);
 	});
-})
+});
