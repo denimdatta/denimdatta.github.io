516450ab6c5691f7d6ba52d208d6bfe2cf20a443
Denim Datta 👻 <d.dat08@gmail.com>
2025-07-18 10:34:39 -0700

    Update Test

--- tst/setup.ts
+++ tst/setup.ts
@@ -1,7 +1,24 @@
 import "@testing-library/jest-dom/vitest"
 import "@testing-library/jest-dom"
-import {afterEach} from 'vitest'
+import {afterAll, afterEach, beforeAll, vi} from 'vitest'
 import {cleanup} from '@testing-library/react'
+
+// create global stubs before all tests
+beforeAll(() => {
+	const IntersectionObserverMock = vi.fn(() => ({
+		disconnect: vi.fn(),
+		observe: vi.fn(),
+		takeRecords: vi.fn(),
+		unobserve: vi.fn(),
+	}));
+
+	vi.stubGlobal('IntersectionObserver', IntersectionObserverMock);
+})
+
+// Clear stubs after all tests
+afterAll(() => {
+	vi.unstubAllGlobals();
+})
 
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
